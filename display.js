877f17b7de07107865e29faaba0155056d247602
mylesmurphy3207 <112994560+mylesmurphy3207@users.noreply.github.com>
2022-10-15 23:36:00 -0700

    10/11/2022

--- display.js
+++ display.js
@@ -1,24 +1,50 @@
 function displayPetCards(){
-        //get the HTML element where we display the pets
-        let petsDiv = document.getElementById("pets");
-        let tmp;
-        for(let i=0;i<petSalon.pets.length;i++){
-            let aPet = petsSalon.pets[i];
-             tmp+=`
-            <div class="pet">
-            
-                <p>Name: ${aPet.name} </p>
-                <p>Age: ${aPet.age} </p>
-                <p>Gender: ${aPet.gender} </p>
-                <p>Breed: ${aPet.breed} </p>
-                <p>Service: ${aPet.service} </p>
-            </div>`;
-            console.log(tmp);
-        }
-        
-        petsDiv.innerHTML=(tmp);
+    //get the html element where we display the pets
+    let petsDiv = document.getElementById("pets");
+    let tmp = "" //tmp stands for  template
+    //travel the pets array
+    for(let i=0; i<petSalon.pets.length;i++){
+
+        let aPet = petSalon.pets[i];
+        tmp+=`
+        <div id="${aPet.id}" class="pet">
+            <p> Name: ${aPet.name} </p>
+            <p> Age: ${aPet.age} </p>
+            <p> Gender: ${aPet.gender} </p>
+            <p> Breed: ${aPet.breed} </p>
+            <p> Color: ${aPet.color} </p>
+            <p> Service: ${aPet.service} </p>
+            <p> Owner's Name: ${aPet.ownerName} </p>
+            <p> Mobile: ${aPet.contactPhone} </p>
+            <button onclick="deletePet(${aPet.id});" class="btn btn-danger"> Delete </button>
+        </div>
+            `;
+    }
+        // creat the template (card)
+    petsDiv.innerHTML = tmp;
+        //insert the card into the html element
+}
+function displayPetTable(){
+
+ let pTable = document.getElementById("tablePets");
+    let format = ""
+        for(let i=0; i<petSalon.pets.length;i++){
+
+            let aPet = petSalon.pets[i];
+            format+=` 
+            <tr id="${aPet.id}"> 
+                <td> ${aPet.name}</td>
+                <td> ${aPet.age}</td>
+                <td> ${aPet.gender}</td>
+                <td> ${aPet.breed}</td>
+                <td> ${aPet.color}</td>
+                <td> ${aPet.service}</td>
+                <td> ${aPet.ownerName}</td>
+                <td> ${aPet.contactPhone}</td>
+                <td><button onclick="deletePet(${aPet.id});" class="btn btn-danger"> Delete </button></td>
+            </tr>
+            `;
+}
+    pTable.innerHTML = format;
 }
 
-function dissplayPetTable(){
-
-}
--- register.js
+++ register.js
@@ -1,72 +1,119 @@
-//object literal
+//object literal {}
 let petSalon = {
-    //atributes
-    name:"Pet fashion",
-    phone: "333-333-333",
+    //attributes
+    name: "Pet Salon",
+    Phone: "850",
     address:{
         country:"USA",
-        city:"san diego",
-        street:"Marlborugh ave",
-        number:"3829",
-        Zip:"92105"
+        city:"SAN DIEGO",
+        street:"MARLBOROUGH",
+        zip:"92105",
     },
     pets:[]
+        
 }
-
-
-
-//these are the arguments
-function Pet(name,age,gender,breed,service,owner,phone){
-//the real attributes are the enxt:
+console.log(petSalon)
+let count = 0;
+//       these are the arguments (local variables)
+                //CONSTRUCTOR
+function Pet(name, age, gender, breed, color, service, owner, phone, payment){
     this.name=name;
     this.age=age;
     this.gender=gender;
     this.breed=breed;
+    this.color=color;
     this.service=service;
     this.ownerName=owner;
-    this.contactPhone=phone
+    this.contactPhone=phone;
+    this.payment=payment
+    this.id=count++;
+}
+// create a new pet 
+
+// push the pet into the array
+
+let inputName=document.getElementById("txtName");
+let inputAge=document.getElementById("txtAge");
+let inputGender=document.getElementById("txtGender"); 
+let inputBreed=document.getElementById("txtBreed");
+let inputColor=document.getElementById("txtColor");
+let inputService=document.getElementById("txtService");
+let inputOwner=document.getElementById("txtOwner");
+let inputMobile=document.getElementById("txtMobile");
+let inputPayment=document.getElementById("txtPayment");
+
+function isValid(newPet){
+    let valid=true;
+    if(newPet.service==""){
+         valid=false;
+    }
+    return valid;
 }
 
-// create a new pet
-let Jiraiya = new Pet("Jiraiya",59,"Male","Dane","Grooming","Shaggy","333-333-3333");
-console.log(Jiraiya);
-let Pain = new Pet("Pain",59,"Male","Dane","Grooming","Shaggy","333-333-3333");
-console.log(Pain);
-
-petSalon.pets.push(Jiraiya,Pain);
-// push the pet nto the array
-let inputName=document.getElementsByID("txtName");
-let inputAge=document.getElementsByID("txtAge");
-let inputGender=document.getElementsByID("txtGender");
-let inputBreed=document.getElementsByID("txtBreed");
-let inputService=document.getElementsByID("txtService");
-let inputOwner=document.getElementsByID("txtOwner");
-let inputContactphone=document.getElementsByID("txtContactphone");
-
 function register(){
-    let thePet = new pet(inputName.value,inputAge.value,inputGender.value,inputBreed,value,selectService.value);
+    console.log(inputName.value,inputAge.value, inputGender.value,inputBreed.value, inputService.value, inputOwner.value, inputMobile.value, inputPayment.value);
+
+// create the object
+    let thePet = new Pet(inputName.value, inputAge.value, inputGender.value, inputBreed.value,inputColor.value, inputService.value, inputOwner.value,inputMobile.value,inputPayment.value);
+
+    if(isValid(thePet)==true){
+        console.log(thePet);
+//push the object into the array
     petSalon.pets.push(thePet);
     clearInputs();
     displayNumberOfPets();
-    }
+    displayPetCards();
+    displayPetTable();
+    } else{ 
+        alert("Enter a service");
+}
+//display the object in the console
+}
 
 function clearInputs(){
-    inputName.value="";
-    inputAge.value="";
-    inputGender.value="";
-    inputBreed.value="";
-    selectService.value="";
+    inputName.value = "";
+    inputAge.value = "";
+    inputGender.value = ""; 
+    inputBreed.value = "";
+    inputColor.value = "";
+    inputService.value = "";
+    inputOwner.value = "";
+    inputMobile.value = "";
+    inputPayment.value = "";
 }
 function displayNumberOfPets(){
-    document.getElementById("numberOfPets").innerHTML= `We have $ {petSalon.pets.length} pets in the system.`;
+    document.getElementById("numberOfPets").innerHTML="We Have " + petSalon.pets.length  +" Pets Registered";
 }
-function init(){
-    //create a new pet
-    let Jiraiya = new Pet("Jiraiya",59,"Male","Dane","Grooming","Shaggy","333-333-3333");
-    let Pain = new Pet("Pain",59,"Male","Dane","Grooming","Shaggy","333-333-3333");
-    petSalon.pets.push(Jiraiya,Pain);
+   //an argument withing a function parenthesis () can be replaced by whatever value the function is trying to affect
+function deletePet(petID){
+    console.log("Deleting a pet ... " + petID);
+    // this is how the function indentifies which index to remove
+    let deleteIndex;
+    for(let i = 0; i < petSalon.pets.length; i++) {
+        let aPet = petSalon.pets[i];
+        if(aPet.id==petID){
+        deleteIndex = i;
+    console.log("The deleted pet is in the position" + deleteIndex); 
+        }
+    }
+    petSalon.pets.splice(deleteIndex, 1); // splice removes an index from the array
+    document.getElementById(petID).remove(); //removes an item from visual HTML
     displayNumberOfPets();
     displayPetTable();
 }
 
+function init(){
+    let cat = new Pet("Jiraiya",67,"Male","Dane","Brown","Grooming","cats",850);
+    let turtle = new Pet("Naruto",12,"Male","Fire","Green","Scaling","Ninja-turtle",763);
+    let ostrich = new Pet("Pain",3, "Male","Bengal","Orange","Grooming","ostrich-ch",763); 
+    petSalon.pets.push(jiraiya, Naruto, Pain);
+    displayNumberOfPets();
+    displayPetCards();
+    displayPetTable();
+}
 window.onload = init;
+//start point; stopping conditon ; increment
+// length return the number of elements in the array
+
+
+
